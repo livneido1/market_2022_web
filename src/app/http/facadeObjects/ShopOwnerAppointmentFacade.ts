5c47933cc6608cd446e25c74b515b7fd90361411
dollshak <dollshak@post.bgu.ac.il>
2022-05-08 23:07:21 +0300

    facade objects

---
+++ src/app/http/facadeObjects/ShopOwnerAppointmentFacade.ts
@@ -0,0 +1,12 @@
+import { Deserializer } from "v8";
+import { Deserializeable } from "./deserializable";
+import { AppointmentFacade } from "./AppointmentFacade";
+
+export class ShopOwnerAppointmentFacade extends AppointmentFacade{
+    constructor(){
+        super()
+    }
+
+
+
+}
