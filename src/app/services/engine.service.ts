b702f4014d7f465755a188f26db9f187f5a82780
Ido Livne <Livneido1@gmail.com>
2022-05-08 21:44:37 +0300

    engine turned to client http

--- src/app/services/engine.service.ts
+++ src/app/services/engine.service.ts
@@ -1,19 +1,24 @@
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EngineService {
+  private serverUrl: string = 'http://localhost:8080';
+  constructor(private http: HttpClient, private headers: HttpHeaders) {}
 
-  constructor(
-    private http : HttpClientModule,
-    private headers: HttpHeaders,
-    ) { }
+  register(request: string): Observable<string> {
+    return this.http.post<string>('/register', request, httpOptions);
+  }
 
-
-
-    
-  
+  guestLogin():Observable<any>{
+    return this.http.post<any>(this.serverUrl + "/guestLogin" , httpOptions)
+  }
 }
