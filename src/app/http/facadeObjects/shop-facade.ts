a6f90b69225029f78819026a1ae2d0c040f9c2aa
Ido Livne <Livneido1@gmail.com>
2022-06-24 02:20:52 +0300

    bidFacade added

--- src/app/http/facadeObjects/shop-facade.ts
+++ src/app/http/facadeObjects/shop-facade.ts
@@ -1,4 +1,5 @@
 import { AppointmentFacade } from './AppointmentFacade';
+import { BidFacade } from './bid-facade';
 import { Deserializable } from './deserializable';
 import { ItemFacade } from './ItemFacade';
 import { ObjectsDeserializer } from './objects-deserializer';
@@ -10,24 +11,32 @@ export class ShopFacade implements Deserializable {
   itemMap: Map<number, ItemFacade>; //<ItemID, actualItem>
   employees: Map<string, AppointmentFacade>; //<name, appointment>
   itemsCurrentAmount: Map<number, number>; // id , amount
+  bidsInShop: BidFacade[];
   closed: boolean;
 
-  constructor() {
-    this.shopName = '';
-    this.itemMap = new Map();
-    this.employees = new Map();
-    this.itemsCurrentAmount = new Map();
-    this.closed = false;
+  constructor(
+    shopName?: string,
+    itemMap?: Map<number, ItemFacade>,
+    employees?: Map<string, AppointmentFacade>,
+    itemsCurrentAmount?: Map<number, number>,
+    bidsInShop?: BidFacade[],
+    closed?: boolean
+  ) {
+    this.shopName = shopName;
+    this.itemMap = itemMap;
+    this.employees = employees;
+    this.itemsCurrentAmount = itemsCurrentAmount;
+    this.bidsInShop = bidsInShop;
+    this.closed = closed;
   }
 
-
-  getItemByName(itemName: string): ItemFacade{
-      for (const item of this.itemMap.values()){
-        if (item.name === itemName){
-          return item;
-        }
+  getItemByName(itemName: string): ItemFacade {
+    for (const item of this.itemMap.values()) {
+      if (item.name === itemName) {
+        return item;
       }
-      return undefined;
+    }
+    return undefined;
   }
 
   deserialize(value: any): this {
@@ -41,7 +50,6 @@ export class ShopFacade implements Deserializable {
       const itemId: number = Number(entry[0]);
       this.itemMap.set(itemId, new ItemFacade().deserialize(entry[1]));
     }
-
 
     //deserialize employees
     const tempApp: AppointmentFacade = new ShopManagerAppointmentFacade();
@@ -58,6 +66,11 @@ export class ShopFacade implements Deserializable {
       const id = Number(entry[0]);
       const amount = Number(entry[1]);
       this.itemsCurrentAmount.set(id, amount);
+    }
+    //deserialzie bids
+    this.bidsInShop = [];
+    for (const bid of value.bidsInShop){
+      this.bidsInShop.push(new BidFacade().deserialize(bid));
     }
     return this;
   }
